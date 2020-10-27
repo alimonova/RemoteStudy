64855f0cc417f125f8ede34920e79e73532c6c76
burchinskaya <sburchinskaya@mail.ru>
2020-10-28 01:14:27 +0200

    Added DB dtos

---
+++ remoteStudy-api/RemoteStudy/Dto/LessonDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RemoteStudy.Dto
+{
+    public class LessonDto
+    {
+        public Guid Id { get; set; }
+        public DateTime DateTimeCreation { get; set; }
+        public string Text { get; set; }
+    }
+}
---
+++ remoteStudy-api/RemoteStudy/Dto/LessonElementDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RemoteStudy.Dto
+{
+    public class LessonElementDto
+    {
+        public Guid Id { get; set; }
+        public string ElementLink { get; set; }
+        public DateTime CreationDateTime { get; set; }
+    }
+}
