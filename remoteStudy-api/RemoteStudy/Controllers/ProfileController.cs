d57c1f8e7e7a822014c5a1a62b95f6e9ef43d976
burchinskaya <sburchinskaya@mail.ru>
2020-12-03 02:28:07 +0200

    Added CRUD functionality for profile

---
+++ remoteStudy-api/RemoteStudy/Controllers/ProfileController.cs
@@ -0,0 +1,56 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using RemoteStudy.Dto;
+using RemoteStudy.Models;
+using RemoteStudy.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace RemoteStudy.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProfileController : ControllerBase
+    {
+        private readonly CurrentUserInfo _info;
+        private readonly IProfileService _profiles;
+        private readonly IMapper _mapper;
+        private readonly IHostingEnvironment _environment;
+
+        public ProfileController(IProfileService profiles, IMapper mapper, CurrentUserInfo info, IHostingEnvironment environment)
+        {
+            _profiles = profiles;
+            _mapper = mapper;
+            _info = info;
+            _environment = environment;
+        }
+
+        [HttpGet("ReadByUserId/{id}")]
+        public IActionResult GetByUserId(Guid id)
+        {
+            var profiles = _profiles.GetProfileByUserId(id);
+            return Ok(_mapper.Map<ProfileDto>(profiles));
+        }
+
+        [HttpGet("ReadById/{id}")]
+        public IActionResult GetById(Guid id)
+        {
+            var profiles = _profiles.GetProfileById(id);
+            return Ok(_mapper.Map<ProfileDto>(profiles));
+        }
+
+
+        [Authorize]
+        [HttpGet("ReadMyInfo")]
+        public IActionResult GetMyInfo()
+        {
+            var profile = _profiles.GetMyInfo();
+            return Ok(_mapper.Map<ProfileDto>(profile));
+        }
+    }
+}
